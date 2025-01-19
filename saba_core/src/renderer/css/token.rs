9873eaa5e4c1c79af8bcc2e2198cba6aaa9de4a3
t2hk <30774256+t2hk@users.noreply.github.com>
2025-01-19 10:55:26 +0000

    第5章 CSSで装飾する CssToken 列挙子の作成 p.211

---
+++ saba_core/src/renderer/css/token.rs
@@ -0,0 +1,29 @@
+use alloc::string::String;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum CssToken {
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-hash-token
+    HashToken(String),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-delim-token
+    Delim(char),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-number-token
+    Munber(f64),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-colon-token
+    Colon,
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-semicolon-token
+    SemiColon,
+    /// https://www.w3.org/TR/css-syntax-3/#tokendef-open-paren
+    OpenParenthesis,
+    /// https://www.w3.org/TR/css-syntax-3/#tokendef-close-paren
+    CloseParenthesis,
+    /// https://www.w3.org/TR/css-syntax-3/#tokendef-open-curly
+    OpenCurly,
+    /// https://www.w3.org/TR/css-syntax-3/#tokendef-close-curly
+    CloseCurly,
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-ident-token
+    Ident(String),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-string-token
+    StringToken(String),
+    /// https://www.w3.org/TR/css-syntax-3/#typedef-at-keyword-token
+    AtKeyword(String),
+}
