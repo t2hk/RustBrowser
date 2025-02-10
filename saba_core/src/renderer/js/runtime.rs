524cbd76a2213a139c17273a2f30fc4e4a8fce7a
t2hk <30774256+t2hk@users.noreply.github.com>
2025-02-10 16:16:53 +0000

    第7章 JavaScript を動かす ランタイムの実装まで完了 p.375

---
+++ saba_core/src/renderer/js/runtime.rs
@@ -0,0 +1,166 @@
+use crate::renderer::js::ast::Node;
+use crate::renderer::js::ast::Program;
+use alloc::rc::Rc;
+use alloc::string::ToString;
+use core::borrow::Borrow;
+use core::ops::Add;
+use core::ops::Sub;
+
+/// ランタイム用の構造体
+#[derive(Debug, Clone)]
+pub struct JsRuntime {}
+
+impl JsRuntime {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    /// program の body に含まれるノードを eval メソッドによってすべて評価する。
+    pub fn execute(&mut self, program: &Program) {
+        for node in program.body() {
+            self.eval(&Some(node.clone()));
+        }
+    }
+
+    /// ノードの種類に応じて処理する。
+    fn eval(&mut self, node: &Option<Rc<Node>>) -> Option<RuntimeValue> {
+        let node = match node {
+            Some(n) => n,
+            None => return None,
+        };
+
+        match node.borrow() {
+            // ExpressionStatement の場合、再帰呼び出し
+            Node::ExpressionStatement(expr) => return self.eval(&expr),
+            // 加算または減算
+            Node::AdditiveExpression {
+                operator,
+                left,
+                right,
+            } => {
+                let left_value = match self.eval(&left) {
+                    Some(value) => value,
+                    None => return None,
+                };
+                let right_value = match self.eval(&right) {
+                    Some(value) => value,
+                    None => return None,
+                };
+
+                if operator == &'+' {
+                    Some(left_value + right_value)
+                } else if operator == &'-' {
+                    Some(left_value - right_value)
+                } else {
+                    None
+                }
+            }
+            Node::AssignmentExpression {
+                operator: _,
+                left: _,
+                right: _,
+            } => {
+                // 後ほど実装
+                None
+            }
+            Node::MemberExpression {
+                object: _,
+                property: _,
+            } => {
+                // 後ほど実装
+                None
+            }
+            Node::NumericLiteral(value) => Some(RuntimeValue::Number(*value)),
+        }
+    }
+}
+
+/// RuntimeValue 列挙型
+/// JavaScript ランタイムで扱う値を表す。
+#[derive(Debug, Clone, PartialEq)]
+pub enum RuntimeValue {
+    /// https://262.ecma-international.org/#sec-numeric-types
+    Number(u64),
+}
+
+/// Add トレイトの実装
+impl Add<RuntimeValue> for RuntimeValue {
+    type Output = RuntimeValue;
+
+    fn add(self, rhs: RuntimeValue) -> RuntimeValue {
+        let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (&self, &rhs);
+        return RuntimeValue::Number(left_num + right_num);
+    }
+}
+
+/// Sub トレイトの実装
+impl Sub<RuntimeValue> for RuntimeValue {
+    type Output = RuntimeValue;
+
+    fn sub(self, rhs: RuntimeValue) -> RuntimeValue {
+        let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (&self, &rhs);
+        return RuntimeValue::Number(left_num - right_num);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::renderer::js::ast::JsParser;
+    use crate::renderer::js::token::JsLexer;
+
+    /// JavaScript に数値のみが含まれる場合のテスト。
+    /// 評価結果として数値 (RuntimeValue::Number) が返るはずである。
+    #[test]
+    fn test_num() {
+        let input = "42".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [Some(RuntimeValue::Number(42))];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()));
+            assert_eq!(expected[i], result);
+            i += 1;
+        }
+    }
+
+    /// 足し算のテスト
+    #[test]
+    fn test_add_nums() {
+        let input = "1 + 2".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [Some(RuntimeValue::Number(3))];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()));
+            assert_eq!(expected[i], result);
+            i += 1;
+        }
+    }
+
+    /// 引き算のテスト
+    #[test]
+    fn test_sub_nums() {
+        let input = "2 - 1".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [Some(RuntimeValue::Number(1))];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()));
+            assert_eq!(expected[i], result);
+            i += 1;
+        }
+    }
+}
