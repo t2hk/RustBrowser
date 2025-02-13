73ca4be07eaf015a37fa89d759e29c6b6f7bd192
t2hk <30774256+t2hk@users.noreply.github.com>
2025-02-13 19:38:07 +0000

    第7章 JavaScript を動かす ランタイムの変更 まで完了 P.401

--- saba_core/src/renderer/js/runtime.rs
+++ saba_core/src/renderer/js/runtime.rs
@@ -1,29 +1,100 @@
 use crate::renderer::js::ast::Node;
 use crate::renderer::js::ast::Program;
+use alloc::format;
 use alloc::rc::Rc;
+use alloc::string::String;
 use alloc::string::ToString;
+use alloc::vec::Vec;
 use core::borrow::Borrow;
+use core::cell::RefCell;
+use core::fmt::Display;
+use core::fmt::Formatter;
 use core::ops::Add;
 use core::ops::Sub;
 
+/// 変数の名前と値のタプルを表す。
+type VariableMap = Vec<(String, Option<RuntimeValue>)>;
+
+/// JavaScript の変数のスコープ管理を行うための構造体
+/// https://262.ecma-international.org/#sec-environment-records
+#[derive(Debug, Clone)]
+pub struct Environment {
+    variables: VariableMap,                  // 変数の名前と値を表す。
+    outer: Option<Rc<RefCell<Environment>>>, // 外部のスコープを表す。内側のスコープからはアクセスできる。
+}
+
+impl Environment {
+    fn new(outer: Option<Rc<RefCell<Environment>>>) -> Self {
+        Self {
+            variables: VariableMap::new(),
+            outer,
+        }
+    }
+
+    /// 変数の取得
+    /// Environemnt 構造体に保存してる変数について、name と一致する変数の値を返す。
+    /// まず variables をチェックし、ローカル変数の中で一致するものがあるかチェックする。
+    /// 見つからない場合、外部のスコープの outer をチェックする。
+    pub fn get_variable(&self, name: String) -> Option<RuntimeValue> {
+        for variable in &self.variables {
+            if variable.0 == name {
+                return variable.1.clone();
+            }
+        }
+
+        if let Some(env) = &self.outer {
+            env.borrow_mut().get_variable(name)
+        } else {
+            None
+        }
+    }
+
+    /// 変数の追加
+    /// 現在のスコープに新しい変数を追加する。
+    fn add_variable(&mut self, name: String, value: Option<RuntimeValue>) {
+        self.variables.push((name, value));
+    }
+
+    /// 変数の更新
+    /// 現在のスコープに存在する変数の値を更新する。
+    fn update_variable(&mut self, name: String, value: Option<RuntimeValue>) {
+        for i in 0..self.variables.len() {
+            // 変数を見つけた場合、現在の変数を削除し、更新後の変数を追加する。
+            if self.variables[i].0 == name {
+                self.variables.remove(i);
+                self.variables.push((name, value));
+                return;
+            }
+        }
+    }
+}
+
 /// ランタイム用の構造体
 #[derive(Debug, Clone)]
-pub struct JsRuntime {}
+pub struct JsRuntime {
+    env: Rc<RefCell<Environment>>,
+}
 
 impl JsRuntime {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            env: Rc::new(RefCell::new(Environment::new(None))),
+        }
     }
 
     /// program の body に含まれるノードを eval メソッドによってすべて評価する。
     pub fn execute(&mut self, program: &Program) {
         for node in program.body() {
-            self.eval(&Some(node.clone()));
+            self.eval(&Some(node.clone()), self.env.clone());
         }
     }
 
     /// ノードの種類に応じて処理する。
-    fn eval(&mut self, node: &Option<Rc<Node>>) -> Option<RuntimeValue> {
+    fn eval(
+        &mut self,
+        node: &Option<Rc<Node>>,
+        env: Rc<RefCell<Environment>>,
+    ) -> Option<RuntimeValue> {
         let node = match node {
             Some(n) => n,
             None => return None,
@@ -31,18 +102,18 @@ impl JsRuntime {
 
         match node.borrow() {
             // ExpressionStatement の場合、再帰呼び出し
-            Node::ExpressionStatement(expr) => return self.eval(&expr),
+            Node::ExpressionStatement(expr) => return self.eval(&expr, env.clone()),
             // 加算または減算
             Node::AdditiveExpression {
                 operator,
                 left,
                 right,
             } => {
-                let left_value = match self.eval(&left) {
+                let left_value = match self.eval(&left, env.clone()) {
                     Some(value) => value,
                     None => return None,
                 };
-                let right_value = match self.eval(&right) {
+                let right_value = match self.eval(&right, env.clone()) {
                     Some(value) => value,
                     None => return None,
                 };
@@ -56,11 +127,21 @@ impl JsRuntime {
                 }
             }
             Node::AssignmentExpression {
-                operator: _,
-                left: _,
-                right: _,
+                operator,
+                left,
+                right,
             } => {
-                // 後ほど実装
+                if operator != &'=' {
+                    return None;
+                }
+                // 変数の再割り当て
+                if let Some(node) = left {
+                    if let Node::Identifier(id) = node.borrow() {
+                        let new_value = self.eval(right, env.clone());
+                        env.borrow_mut().update_variable(id.to_string(), new_value);
+                        return None;
+                    }
+                }
                 None
             }
             Node::MemberExpression {
@@ -71,7 +152,27 @@ impl JsRuntime {
                 None
             }
             Node::NumericLiteral(value) => Some(RuntimeValue::Number(*value)),
-            _ => todo!(),
+            Node::VariableDeclaration { declarations } => {
+                for declaration in declarations {
+                    self.eval(&declaration, env.clone());
+                }
+                None
+            }
+            Node::VariableDeclarator { id, init } => {
+                if let Some(node) = id {
+                    if let Node::Identifier(id) = node.borrow() {
+                        let init = self.eval(&init, env.clone());
+                        env.borrow_mut().add_variable(id.to_string(), init);
+                    }
+                }
+                None
+            }
+            Node::Identifier(name) => match env.borrow_mut().get_variable(name.to_string()) {
+                Some(v) => Some(v),
+                None => Some(RuntimeValue::StringLiteral(name.to_string())),
+            },
+            Node::StringLiteral(value) => Some(RuntimeValue::StringLiteral(value.to_string())),
+            // _ => todo!(),
         }
     }
 }
@@ -82,6 +183,7 @@ impl JsRuntime {
 pub enum RuntimeValue {
     /// https://262.ecma-international.org/#sec-numeric-types
     Number(u64),
+    StringLiteral(String),
 }
 
 /// Add トレイトの実装
@@ -89,8 +191,10 @@ impl Add<RuntimeValue> for RuntimeValue {
     type Output = RuntimeValue;
 
     fn add(self, rhs: RuntimeValue) -> RuntimeValue {
-        let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (&self, &rhs);
-        return RuntimeValue::Number(left_num + right_num);
+        if let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (&self, &rhs) {
+            return RuntimeValue::Number(left_num + right_num);
+        }
+        RuntimeValue::StringLiteral(self.to_string() + &rhs.to_string())
     }
 }
 
@@ -99,8 +203,22 @@ impl Sub<RuntimeValue> for RuntimeValue {
     type Output = RuntimeValue;
 
     fn sub(self, rhs: RuntimeValue) -> RuntimeValue {
-        let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (&self, &rhs);
-        return RuntimeValue::Number(left_num - right_num);
+        if let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (&self, &rhs) {
+            return RuntimeValue::Number(left_num - right_num);
+        }
+        // NaN
+        RuntimeValue::Number(u64::MIN)
+    }
+}
+
+/// RuntimeValue の値を文字列に変換する。
+impl Display for RuntimeValue {
+    fn fmt(&self, f: &mut Formatter) -> core::fmt::Result {
+        let s = match self {
+            RuntimeValue::Number(value) => format!("{}", value),
+            RuntimeValue::StringLiteral(value) => value.to_string(),
+        };
+        write!(f, "{}", s)
     }
 }
 
@@ -123,7 +241,7 @@ mod tests {
         let mut i = 0;
 
         for node in ast.body() {
-            let result = runtime.eval(&Some(node.clone()));
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
             assert_eq!(expected[i], result);
             i += 1;
         }
@@ -141,7 +259,7 @@ mod tests {
         let mut i = 0;
 
         for node in ast.body() {
-            let result = runtime.eval(&Some(node.clone()));
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
             assert_eq!(expected[i], result);
             i += 1;
         }
@@ -159,7 +277,67 @@ mod tests {
         let mut i = 0;
 
         for node in ast.body() {
-            let result = runtime.eval(&Some(node.clone()));
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
+            assert_eq!(expected[i], result);
+            i += 1;
+        }
+    }
+
+    /// 変数定義のテスト
+    /// var foo=42; を入力するテストを行う。
+    /// Program の body に含まれる文を評価すると、None が返るはずである(変数の定義式自体は値を返さない)。
+    #[test]
+    fn test_assign_variable() {
+        let input = "var foo=42;".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [None];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
+            assert_eq!(expected[i], result);
+            i += 1;
+        }
+    }
+
+    /// 変数呼び出しのテスト
+    /// var foo=42; foo+1 を入力するテストを行う。
+    /// Program の body に含まれる文を評価すると、最初の文は None, 次の文は 43 の値を含む RuntimeValue になるはずである。
+    #[test]
+    fn test_add_variable_and_num() {
+        let input = "var foo=42; foo+1".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [None, Some(RuntimeValue::Number(43))];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
+            assert_eq!(expected[i], result);
+            i += 1;
+        }
+    }
+
+    /// 変数変換のテスト
+    /// var foo=42; foo=1; foo を入力するテストを行う。
+    /// Program の body に含まれる文を評価すると、最初の文は None, 次の文は None, 最後は 1 の値を含む RuntimeValue になるはずである。
+    #[test]
+    fn test_reassign_variable() {
+        let input = "var foo=42; foo=1; foo".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [None, None, Some(RuntimeValue::Number(1))];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
             assert_eq!(expected[i], result);
             i += 1;
         }
