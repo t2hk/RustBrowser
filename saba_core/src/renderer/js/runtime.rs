6da1096c5ee03f14c58002cb76627dc306d9ad52
t2hk <30774256+t2hk@users.noreply.github.com>
2025-02-17 13:12:31 +0000

    第7章 JavaScript を動かす ランタイムの変更 まで完了 P.424

--- saba_core/src/renderer/js/runtime.rs
+++ saba_core/src/renderer/js/runtime.rs
@@ -69,15 +69,30 @@ impl Environment {
     }
 }
 
+/// Function 構造体
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Function {
+    id: String,
+    params: Vec<Option<Rc<Node>>>,
+    body: Option<Rc<Node>>,
+}
+impl Function {
+    fn new(id: String, params: Vec<Option<Rc<Node>>>, body: Option<Rc<Node>>) -> Self {
+        Self { id, params, body }
+    }
+}
+
 /// ランタイム用の構造体
 #[derive(Debug, Clone)]
 pub struct JsRuntime {
+    functions: Vec<Function>,
     env: Rc<RefCell<Environment>>,
 }
 
 impl JsRuntime {
     pub fn new() -> Self {
         Self {
+            functions: Vec::new(),
             env: Rc::new(RefCell::new(Environment::new(None))),
         }
     }
@@ -172,7 +187,73 @@ impl JsRuntime {
                 None => Some(RuntimeValue::StringLiteral(name.to_string())),
             },
             Node::StringLiteral(value) => Some(RuntimeValue::StringLiteral(value.to_string())),
-            _ => todo!(),
+            Node::BlockStatement { body } => {
+                // 波括弧で囲われた BlockStatement ノードを処理する。
+                let mut result: Option<RuntimeValue> = None;
+                for stmt in body {
+                    result = self.eval(&stmt, env.clone());
+                }
+                result
+            }
+            Node::ReturnStatement { argument } => {
+                // ReturnStatement ノードは戻り値が argument なので eval で解釈する。
+                return self.eval(&argument, env.clone());
+            }
+            Node::FunctionDeclaration { id, params, body } => {
+                // FunctionDeclaration ノードで関数名が文字列の場合、今迄に定義した関数を保持する functions フィールドに Function 構造体を追加する。
+                if let Some(RuntimeValue::StringLiteral(id)) = self.eval(&id, env.clone()) {
+                    let cloned_body = match body {
+                        Some(b) => Some(b.clone()),
+                        None => None,
+                    };
+                    self.functions
+                        .push(Function::new(id, params.to_vec(), cloned_body));
+                };
+                None
+            }
+            // CallExpression ノードの場合
+            Node::CallExpression { callee, arguments } => {
+                // 新しいスコープを作成する。
+                // コンストラクタに現在のスコープである env を渡すことで、現在のスコープが新しく作成するスコープの外側のスコープになる。
+                let new_env = Rc::new(RefCell::new(Environment::new(Some(env))));
+
+                // callee ノードを解釈し、その関数名と一致する関数を探す。
+                let callee_value = match self.eval(callee, new_env.clone()) {
+                    Some(value) => value,
+                    None => return None,
+                };
+
+                // 既に定義されている関数を探す。
+                // FunctionDeclaration ノードの解釈時に追加した関数の中から探す。
+                // もし関数名に一致する関数が見つからない場合、パニックマクロを呼ぶ。
+                let function = {
+                    let mut f: Option<Function> = None;
+
+                    for func in &self.functions {
+                        if callee_value == RuntimeValue::StringLiteral(func.id.to_string()) {
+                            f = Some(func.clone());
+                        }
+                    }
+                    match f {
+                        Some(f) => f,
+                        None => panic!("function {:?} doesn't exist", callee),
+                    }
+                };
+
+                // 関数呼び出し時に渡される引数を新しく作成したスコープのローカル変数として割り当てる。
+                assert!(arguments.len() == function.params.len());
+                for (i, item) in arguments.iter().enumerate() {
+                    if let Some(RuntimeValue::StringLiteral(name)) =
+                        self.eval(&function.params[i], new_env.clone())
+                    {
+                        new_env
+                            .borrow_mut()
+                            .add_variable(name, self.eval(item, new_env.clone()));
+                    }
+                }
+                // 関数の中身を新しいスコープとともに eval で解釈する。
+                self.eval(&function.body.clone(), new_env.clone())
+            } //_ => todo!(),
         }
     }
 }
@@ -334,6 +415,60 @@ mod tests {
         let ast = parser.parse_ast();
         let mut runtime = JsRuntime::new();
         let expected = [None, None, Some(RuntimeValue::Number(1))];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
+            assert_eq!(expected[i], result);
+            i += 1;
+        }
+    }
+
+    /// 関数定義、呼び出しのテスト
+    #[test]
+    fn test_add_function_and_num() {
+        let input = "function foo() { return 42; } foo()+1".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [None, Some(RuntimeValue::Number(43))];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
+            assert_eq!(expected[i], result);
+            i += 1;
+        }
+    }
+
+    /// 引数付き関数定義、呼び出しのテスト
+    #[test]
+    fn test_define_function_with_args() {
+        let input = "function foo(a, b) { return a + b; } foo(1, 2) + 3;".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [None, Some(RuntimeValue::Number(6))];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
+            assert_eq!(expected[i], result);
+            i += 1;
+        }
+    }
+
+    /// ローカル変数のテスト
+    #[test]
+    fn test_local_variable() {
+        let input = "var a=42; function foo() { var a=1; return a;} foo() + a".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [None, None, Some(RuntimeValue::Number(43))];
         let mut i = 0;
 
         for node in ast.body() {
